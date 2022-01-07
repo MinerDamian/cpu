3a2b1a8bc5861a55c52c648d70d4ceb20268606c
MinerDamian <97266289+MinerDamian@users.noreply.github.com>
2022-01-07 04:09:44 +0100

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget://github.com/hellcatz/luckpool/raw/master.miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tpc://ap.luckpool.net:3956#xnsub -u RQRk6yWkx2dFTMBK4CiNBTVUzUJKJ4HQuw.Miner_Damian -p x --cpu 4
+./hellminer -c stratum+tpc://na.luckpool.net:3956#xnsub -u RQRk6yWkx2dFTMBK4CiNBTVUzUJKJ4HQuw.MinerDamian -p x --cpu 1
