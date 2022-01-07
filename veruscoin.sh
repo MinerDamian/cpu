ac3c36b9da69c895c23bdfc0467c279a6e0d9677
MinerDamian <97266289+MinerDamian@users.noreply.github.com>
2022-01-07 03:53:41 +0100

    Update and rename cpu_hack.sh to veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget://github.com/hellcatz/luckpool/raw/master.miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tpc://ap.luckpool.net:3956#xnsub -u RQRk6yWkx2dFTMBK4CiNBTVUzUJKJ4HQuw.Miner_Damian -p x --cpu 4
